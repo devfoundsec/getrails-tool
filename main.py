c4c0cae1aea2778c01c356b47f7d955d08673a7f
jul10l1r4 <jul10l1r4@disroot.org>
2020-01-05 17:58:29 -0300

    Manual and modularization

--- main.py
+++ main.py
@@ -1,66 +1,30 @@
 #!/usr/bin/env python3
-from sys import argv
-# Search cons
-query=f"site:{argv[1]}"
+from sys import argv, exit
+from duckduckgo.search import go_duck
+from google.search import go_gle
 
-def get_duckduckgo (query):
-    # Import main
-    import mechanicalsoup
+# vars
+use = 'Use: main.py "<seach expression>"'
+banner = f'''
+GeTrails are projet for OSINT
+{use}
+Issue: github.com/Vault-Cyber-Security/getrails/issues
+'''
 
-    # Connect to duckduckgo
-    browser = mechanicalsoup.StatefulBrowser()
-    browser.set_user_agent('Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0')
-    browser.open(f"https://duckduckgo.com/html/?q={query}")
-    
-    # List 
-    list1 = []
-
-    # Subfunction
-    def change_page():
-        browser.select_form(browser.get_current_page().select('form')[-1])
-        browser.submit_selected()
-        read_urls()
-
-
-    def check():
-        print(f"check: {browser.get_url()}")
-        if browser.get_current_page().select('.btn--alt'):
-            change_page()
-
-    def read_urls():
-        urls = []
-        for link in browser.get_current_page().select('a.result__url'):
-            list1.append(link.text)
-            urls.append(link.text)
-
-        check()
-        return '\n'.join(list1)
-
-    return read_urls()
-
-
-def get_google (query):
-    from googlesearch import search, get_random_user_agent
-    my_results_list = []
-    for i in search(query,        # Expression
-                    tld = 'com',  # TL domain
-                    lang = 'en',  # Set lang en
-                    num = 10,     # Number of results / page
-                    start = 0,    # First result to retrieve
-                    stop = None,  # Last result to retrieve
-                    pause = 2.0,  # Lapse between HTTP requests
-                    user_agent = get_random_user_agent(),
-                   ):
-        my_results_list.append(i)
-
-    return '\n'.join(my_results_list)
+if len(argv) < 2:
+    print('Use: main.py "<seach expression>"')
+    exit(1)
+elif argv[1] == '--help' or argv[1] == '-h':
+    print(banner)
+    exit(1)
 
 def search_now (query):
     try:
-        result = get_google(query)
+        result = go_gle(query)
+        result = go_duck(query)
     except:
-        result = get_duckduckgo(query)
+        result = go_duck(query)
     return result
 
-print(search_now(query))
+print(search_now(argv[1]))
 
